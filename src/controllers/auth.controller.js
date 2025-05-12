ee4e36259d74113b69231f3633cdb692158f6e83
alvaromyv <alvaromoyanovila@gmail.com>
2025-05-12 19:32:18 +0200

    Mensaje de error en acceder actualizado.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -11,9 +11,9 @@ exports.acceder = async (req, res) => {
     const sql = "SELECT * FROM admins WHERE email = ? AND password = ?"
     pool.query(sql,[email, password], (err, result, fields) => {
         if(err || result.length === 0) {
-            return res.status(401).json({ message: "El correo o contraseña no son correctos."})
+            return res.status(401).json({ message: "El correo y/o la contraseña no son correctos."})
         }
-        
+
         res.status(200).json(result[0])
     })
 };
